2ec54e8cde7f2b35813697fe25c40406d5782513
Sergey <sergsmile29@gmail.com>
2022-04-30 00:08:44 +0300

    fixed comics

--- src/components/singleComic/SingleComic.js
+++ src/components/singleComic/SingleComic.js
@@ -1,38 +1,42 @@
 import './singleComic.scss';
-import xMen from '../../resources/img/x-men.png';
 import useMarvelService from "../../services/MarvelService";
 import {useEffect, useState} from "react";
+import Spinner from "../spinner/spinner";
 
 const SingleComic = () => {
 
-    const [comics, setComics] = useState([]);
-    const [th,setTh] = useState('')
+    const [comics, setComics] = useState(null);
     const {getSingleComics} = useMarvelService();
     const onRequest= () => {
-        getSingleComics(321)
+        getSingleComics(31)
             .then(res => {
                 setComics(res)
-                setTh(res.thumbnail.path + res.thumbnail.extension)
             })
     }
     useEffect(() => {
         onRequest()
-
     }, []);
 
-
-    return (
-        <div className="single-comic">
-            <img src={th} alt="x-men" className="single-comic__img"/>
+const RenderComics = () =>{
+    return (<div className="single-comic">  <img src={comics.thumbnail.path + '.'+ comics.thumbnail.extension} alt="x-men" className="single-comic__img"/>
             <div className="single-comic__info">
                 <h2 className="single-comic__name">{comics.title}</h2>
                 <p className="single-comic__descr">{comics.description}</p>
                 <p className="single-comic__descr">144 pages</p>
                 <p className="single-comic__descr">Language: en-us</p>
-                <div className="single-comic__price">9.99$</div>
+                <div className="single-comic__price">{comics.prices[0].price}$</div>
             </div>
             <a href="#" className="single-comic__back">Back to all</a>
         </div>
+
+    )
+}
+    const content = comics ? <RenderComics/> : <Spinner/>
+    return (
+        <>
+            { content }
+        </>
+
     )
 }
 
