7cdb9315e040343ee9b1fa26ce2e20ff7694db05
Sergey <sergsmile29@gmail.com>
2022-05-19 01:13:03 +0300

    added style for active buttons

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,5 +1,5 @@
 import './appHeader.scss';
-import {Link} from "react-router-dom";
+import {NavLink, Link} from "react-router-dom";
 
 const AppHeader = () => {
     return (
@@ -11,9 +11,12 @@ const AppHeader = () => {
             </h1>
             <nav className="app__menu">
                 <ul>
-                    <li><Link to="/">Characters</Link></li>
+                    {/*Для добавления стиля активной ссылки в react-router-dom v6 используется:
+                    end style={({ isActive }) => ({ color: isActive ? '#9f0013' : 'inherit' })}
+                    */}
+                    <li><NavLink end style={({ isActive }) => ({ color: isActive ? '#9f0013' : 'inherit' })} to="/">Characters</NavLink></li>
                     /
-                    <li><Link to="/comics">Comics</Link></li>
+                    <li><NavLink end style={({ isActive }) => ({ color: isActive ? '#9f0013' : 'inherit' })} to="/comics">Comics</NavLink></li>
                 </ul>
             </nav>
         </header>
